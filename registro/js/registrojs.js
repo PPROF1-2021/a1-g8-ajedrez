3540b332bdba23f3b62f4379cd585900499e9d45
Fran150620 <fran1506alcorta@gmail.com>
2021-10-26 12:17:29 -0300

    cambios varios

--- registro/js/registrojs.js
+++ registro/js/registrojs.js
@@ -1,14 +1,14 @@
 function validar() {
-	var nombres, apellidos, correo, contraseña, expresion;
-	nombres = document.getElementById("nombres").value;
-	apellidos = document.getElementById("apellidos").value;
+	var nombre, apellido, correo, contraseña, expresion;
+	nombre = document.getElementById("nombre").value;
+	apellido = document.getElementById("apellido").value;
 	correo = document.getElementById("correo").value;
 	contraseña = document.getElementById("contraseña").value;
 	
 
 	expresion = /\w+@\w+\.+[a-z]/;
 
-	if (nombres === "" || apellidos === ""  || correo === "" || contraseña === "" ){
+	if (nombre === "" || apellido === ""  || correo === "" || contraseña === "" ){
 		alert("Todos los campos son obligatorios");
 		return false;
 	}
@@ -16,8 +16,8 @@ function validar() {
 		alert("El nombre es muy largo");
 		return false;
 	}
-	else if (apellidos.length>80){
-		alert("Los apellidos son muy largos");
+	else if (apellido.length>80){
+		alert("El apellido es muy largo");
 		return false;
 	}
 	else if (correo.length>100){
